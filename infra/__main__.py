79182a8767ca09f89336e824710f9e5f68b76314
Zhe Ming <79309030+zmchang3009@users.noreply.github.com>
2025-09-27 03:40:19 +0000

    Update __main__.py

--- infra/__main__.py
+++ infra/__main__.py
@@ -2,72 +2,69 @@ import pulumi
 import pulumi_aws as aws
 
 # --- Configuration ---
-stack = pulumi.get_stack()  # pulumi.dev
-# from Pulumi.yaml: name: Start-Pulumi-with-EC2
-project_name = pulumi.get_project()
+stack = pulumi.get_stack() # pulumi.dev
+project_name = pulumi.get_project() # from Pulumi.yaml: name: Start-Pulumi-with-EC2
 
 config = pulumi.Config('workshop-config')
 org = config.require('org')
 
 aws_config = pulumi.Config('aws')
-aws_environment = aws_config.require_object(
-    'defaultTags')['tags']['Environment']
+aws_environment = aws_config.require_object('defaultTags')['tags']['Environment']
 aws_managed_by = aws_config.require_object('defaultTags')['tags']['ManagedBy']
 aws_git_repo = aws_config.require_object('defaultTags')['tags']['GitRepo']
 
 # --- 1. Create VPC and Networking ---
 vpc = aws.ec2.Vpc("app-vpc",
-                  cidr_block="10.0.0.0/16",
-                  enable_dns_hostnames=True,
-                  tags={"Name": f"{org}-{stack}-{project_name}-vpc"})
+    cidr_block="10.0.0.0/16",
+    enable_dns_hostnames=True,
+    tags={"Name": f"{org}-{stack}-{project_name}-vpc"})
 
 internet_gateway = aws.ec2.InternetGateway("app-igw",
-                                           vpc_id=vpc.id,
-                                           tags={"Name": f"{org}-{stack}-{project_name}-igw"})
+    vpc_id=vpc.id,
+    tags={"Name": f"{org}-{stack}-{project_name}-igw"})
 
 route_table = aws.ec2.RouteTable("app-rt",
-                                 vpc_id=vpc.id,
-                                 routes=[
-                                     aws.ec2.RouteTableRouteArgs(
-                                         cidr_block="0.0.0.0/0",
-                                         gateway_id=internet_gateway.id,
-                                     ),
-                                 ],
-                                 tags={"Name": f"{org}-{stack}-{project_name}-rt"})
+    vpc_id=vpc.id,
+    routes=[
+        aws.ec2.RouteTableRouteArgs(
+            cidr_block="0.0.0.0/0",
+            gateway_id=internet_gateway.id,
+        ),
+    ],
+    tags={"Name": f"{org}-{stack}-{project_name}-rt"})
 
 subnet = aws.ec2.Subnet("app-subnet",
-                        vpc_id=vpc.id,
-                        cidr_block="10.0.1.0/24",
-                        map_public_ip_on_launch=True,
-                        tags={"Name": f"{org}-{stack}-{project_name}-subnet"})
+    vpc_id=vpc.id,
+    cidr_block="10.0.1.0/24",
+    map_public_ip_on_launch=True,
+    tags={"Name": f"{org}-{stack}-{project_name}-subnet"})
 
 route_table_association = aws.ec2.RouteTableAssociation("app-rta",
-                                                        subnet_id=subnet.id,
-                                                        route_table_id=route_table.id)
+    subnet_id=subnet.id,
+    route_table_id=route_table.id)
 
 # --- 2. Create Security Group ---
 security_group = aws.ec2.SecurityGroup("web-sg",
-                                       description="Allow HTTP access to EC2 instance",
-                                       vpc_id=vpc.id,
-                                       ingress=[
-                                           aws.ec2.SecurityGroupIngressArgs(
-                                               from_port=80, to_port=80, protocol="tcp", cidr_blocks=["0.0.0.0/0"],
-                                           ),
-                                       ],
-                                       egress=[
-                                           aws.ec2.SecurityGroupEgressArgs(
-                                               from_port=0, to_port=0, protocol="-1", cidr_blocks=["0.0.0.0/0"],
-                                           ),
-                                       ],
-                                       tags={"Name": f"{org}-{stack}-{project_name}-security-group"})
+    description="Allow HTTP access to EC2 instance",
+    vpc_id=vpc.id,
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=80, to_port=80, protocol="tcp", cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    egress=[
+        aws.ec2.SecurityGroupEgressArgs(
+            from_port=0, to_port=0, protocol="-1", cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    tags={"Name": f"{org}-{stack}-{project_name}-security-group"})
 
 # --- 3. Get Latest Amazon Linux AMI ---
 ami = aws.ec2.get_ami(
     most_recent=True,
     owners=["amazon"],
     filters=[
-        aws.ec2.GetAmiFilterArgs(name="name", values=[
-                                 "amzn2-ami-hvm-*-x86_64-gp2"]),
+        aws.ec2.GetAmiFilterArgs(name="name", values=["amzn2-ami-hvm-*-x86_64-gp2"]),
         aws.ec2.GetAmiFilterArgs(name="virtualization-type", values=["hvm"]),
     ])
 
@@ -174,14 +171,14 @@ sed -i "s/MANAGED_BY_PLACEHOLDER/$MANAGED_BY/g" /var/www/html/index.html
 
 # --- 5. Create EC2 Instance ---
 ec2_instance = aws.ec2.Instance("web-server-instance",
-                                instance_type="t2.micro",
-                                ami=ami.id,
-                                subnet_id=subnet.id,
-                                vpc_security_group_ids=[security_group.id],
-                                user_data=user_data_script,
-                                tags={
-                                    "Name": f"{org}-{stack}-{project_name}-instance",
-                                })
+    instance_type="t2.small",
+    ami=ami.id,
+    subnet_id=subnet.id,
+    vpc_security_group_ids=[security_group.id],
+    user_data=user_data_script,
+    tags={
+        "Name": f"{org}-{stack}-{project_name}-instance",
+    })
 
 # --- Outputs ---
 # pulumi.export("github_org", github_org)
@@ -189,5 +186,4 @@ ec2_instance = aws.ec2.Instance("web-server-instance",
 pulumi.export("instance_id", ec2_instance.id)
 pulumi.export("public_ip", ec2_instance.public_ip)
 pulumi.export("public_dns", ec2_instance.public_dns)
-pulumi.export("web_url", ec2_instance.public_ip.apply(
-    lambda ip: f"http://{ip}"))
+pulumi.export("web_url", ec2_instance.public_ip.apply(lambda ip: f"http://{ip}"))
